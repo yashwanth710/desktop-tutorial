f8084d661b81ff2031e227ee36815f6ace742188
yashwanth710 <betha.yashwanth@gmail.com>
2024-05-15 15:30:21 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -1,6 +1,7 @@
 print("welcome to git")
 
 
+<<<<<<< Updated upstream
     preprocessed_data['TESTEDBY'] = data_json['sampled_by']
     preprocessed_data['VALUETEXT'] = preprocessed_data['VALUETEXT'].replace({'NIL': 0, 'BDL': 0, 'N/d' : 0, 'ND' : 0, 'NA' : 0, 'Nil' : 0, '_' : 0})
     preprocessed_data['TESTTYPE'] = "RoutineTest"
@@ -9,3 +10,15 @@ print("welcome to git")
     # Maintaing the template column order 
     cols = ['SNO','ASSETNAME','TAGNAME','DISPLAYNAME', 'UOM','VALUETEXT', 'TESTMETHOD','SUBSYSTEM','TESTTYPE','SAMPLEDATE', 
             'TESTINGDATE', 'SAMPLEDBY', 'POSITION','POSITION(OTHER)','TESTEDBY','SAMPLINGMETHOD','REASONOFSAMPLING',
+=======
+
+preprocessed_data['TESTEDBY'] = data_json['sampled_by']
+preprocessed_data['VALUETEXT'] = preprocessed_data['VALUETEXT'].replace({'NIL': 0, 'BDL': 0, 'N/d' : 0, 'ND' : 0, 'NA' : 0, 'Nil' : 0, '_' : 0})
+preprocessed_data['TESTTYPE'] = "RoutineTest"
+preprocessed_data['SAMPLEDATE'] = pd.to_datetime(preprocessed_data['SAMPLEDATE'])
+preprocessed_data['TESTINGDATE'] = pd.to_datetime(preprocessed_data['TESTINGDATE'])
+
+
+print('hi hello rmes ')
+I am Betha Yashwanth .....................!!!!!!!!!!!!!!
+>>>>>>> Stashed changes
