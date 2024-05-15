2398b9da6f1d80b83c07580ba193a680edc9a480
yashwanth710 <betha.yashwanth@gmail.com>
2024-05-15 17:48:29 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -4,7 +4,7 @@ print("welcome to my account")
 
 
 preprocessed_data['TESTEDBY'] = data_json['sampled_by']
-preprocessed_data['VALUETEXT'] = preprocessed_data['VALUETEXT'].replace({'NIL': 0, 'BDL': 0, 'N/d' : 0, 'ND' : 0, 'NA' : 0, 'Nil' : 0, '_' : 0})
+preprocessed_data['VALUETEXT'] = preprocessed_data['VALUETEXT'].replace({'NIL': 0, 'BDL': 99, 'N/d' : 0, 'ND' : 1, 'NA' : 0-1, 'Nil' : 0, '_' : 0})
 preprocessed_data['TESTTYPE'] = "RoutineTest"
 preprocessed_data['SAMPLEDATE'] = pd.to_datetime(preprocessed_data['SAMPLEDATE'])
 preprocessed_data['TESTINGDATE'] = pd.to_datetime(preprocessed_data['TESTINGDATE'])
